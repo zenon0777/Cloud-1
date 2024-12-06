8004063d9560d7a66e90a604db0de3a0b47c4e43
zenon0777 <abderrahmane.daifi@protonmail.com>
2024-12-06 12:41:03 +0100

    chore: Update setup_inception.sh to install git during setup

--- terraform/setup_inception.sh
+++ terraform/setup_inception.sh
@@ -3,20 +3,14 @@
 apt-get update
 apt-get upgrade -y
 
-apt-get install -y \
-    ca-certificates \
-    curl \
-    gnupg \
-    lsb-release \
-    make \
+apt-get install -y ca-certificates curl gnupg lsb-release make git
 
 mkdir -p /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 
-
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
-  $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
+  $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list >/dev/null
 
 apt-get update
 
